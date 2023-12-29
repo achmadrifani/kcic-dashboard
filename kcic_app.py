f3486de37c03d9a9e5943385a96c0ac5baca067c
Rifani <achmad.rifani.93@gmail.com>
2023-12-29 17:31:03 +0700

    Separate function to retrieve ndf and present weather data.

--- kcic_app.py
+++ kcic_app.py
@@ -13,98 +13,39 @@ st.set_page_config(layout="wide")
 NDF_FOLDER = "src/ndf"
 
 
-def download_ndf_kecamatan(province):
-    """Mengambil data ndf dan menyimpan kedalam local untuk efisiensi request ke server"""
-    url = f"http://202.90.198.220/MEWS/CSV/kecamatanforecast-{province}.csv"
-
-    if not os.path.exists(NDF_FOLDER):
-        os.makedirs(NDF_FOLDER)
-
-    file_name = os.path.join(NDF_FOLDER, f"kecamatanforecast-{province}.csv")
-
-    response = requests.get(url)
-    if response.status_code == 200:
-        with open(file_name, 'wb') as file:
-            file.write(response.content)
-        print(f"Data for {province} downloaded to {file_name}")
-    else:
-        print(f"Failed downloading data {province}. Status code: {response.status_code}")
-    return file_name
-
-def download_presentwx(province):
+def download_data():
     ftp_host = 'publik.bmkg.go.id'
     ftp_user = 'amandemen'
     ftp_password = 'bmkg2303'  # Ganti dengan kata sandi yang sesuai
-    ftp_file_path = f'/data/presentweather-{province}.csv'
+    pwx_ftp_path = f'/data/kcic/kcic_presentwx.csv'
+    ndf_ftp_path = f'/data/kcic/kcic_ndf.csv'
 
     # Lokasi lokal untuk menyimpan file yang diunduh
-    local_file_path = f'src/ndf/presentweather-{province}.csv'
+    pwx_local_path = f'src/ndf/kcic_presentwx.csv'
+    ndf_local_path = f'src/ndf/kcic_ndf.csv'
 
     # Buat koneksi ke FTP server
     ftp = FTP(ftp_host)
     ftp.login(user=ftp_user, passwd=ftp_password)
 
     # Buka file di mode binary dan unduh ke lokal
-    with open(local_file_path, 'wb') as local_file:
-        ftp.retrbinary(f"RETR {ftp_file_path}", local_file.write)
+    with open(pwx_local_path, 'wb') as local_file:
+        ftp.retrbinary(f"RETR {pwx_ftp_path}", local_file.write)
+
+    with open(ndf_local_path, 'wb') as local_file:
+        ftp.retrbinary(f"RETR {ndf_ftp_path}", local_file.write)
 
     # Tutup koneksi FTP
     ftp.quit()
-    return local_file_path
-
-def merge_ndf_kecamatan():
-    dfs = []
-    for province in ['jakarta', 'jawabarat']:
-        fname = download_ndf_kecamatan(province)
-        df = pd.read_csv(fname, sep=";",
-                         names=["AREA_ID", "DATE", "TMIN", "TMAX", "HUMIN", "HUMAX", "HU", "T", "WEATHER", "WD", "WS"])
-        dfs.append(df)
-    df_ndf = pd.concat(dfs, ignore_index=True)
-    df_sta = pd.read_csv("src/track_ndf.csv")
-
-    dfs = []
-    for id, name, lon, lat in zip(df_sta['id'], df_sta['name'], df_sta['lon'], df_sta['lat']):
-        df = df_ndf.loc[df_ndf['AREA_ID'] == id]
-        df["NAME"] = name
-        df["LON"] = lon
-        df["LAT"] = lat
-        df = df[['NAME', 'LON', 'LAT', 'DATE', 'TMIN', 'TMAX', 'HUMIN', 'HUMAX', 'HU', 'T', 'WEATHER', 'WD', 'WS']]
-        dfs.append(df)
-    df_final = pd.concat(dfs, ignore_index=True)
-    df_final['DATE'] = df_final['DATE'].astype('datetime64[ns]')
-    df_final.to_csv("kcic_forecast.csv", sep=";", index=False)
-    return df_final
-
-def merge_presentwx():
-    dfs = []
-    for province in ['jakarta', 'jawabarat']:
-        fname = download_presentwx(province)
-        df = pd.read_csv(fname, sep=";",
-                         names=["AREA_ID", "DATE", "TMIN", "TMAX", "HUMIN", "HUMAX", "HU", "T", "WEATHER", "WD", "WS"])
-        dfs.append(df)
-    df_ndf = pd.concat(dfs, ignore_index=True)
-    df_sta = pd.read_csv("src/track_ndf.csv")
-
-    dfs = []
-    for id, name, lon, lat in zip(df_sta['id'], df_sta['name'], df_sta['lon'], df_sta['lat']):
-        df = df_ndf.loc[df_ndf['AREA_ID'] == id]
-        df["NAME"] = name
-        df["LON"] = lon
-        df["LAT"] = lat
-        df = df[['NAME', 'LON', 'LAT', 'DATE', 'TMIN', 'TMAX', 'HUMIN', 'HUMAX', 'HU', 'T', 'WEATHER', 'WD', 'WS']]
-        dfs.append(df)
-    df_final = pd.concat(dfs, ignore_index=True)
-    df_final['DATE'] = df_final['DATE'].astype('datetime64[ns]')
-    df_final.to_csv("kcic_presentwx.csv", sep=";", index=False)
-    return df_final
+    return pwx_local_path, ndf_local_path
 
 
-def load_forecast_data():
-    df = pd.read_csv("kcic_forecast.csv", sep=";", parse_dates=["DATE"])
+def load_forecast_data(ndf_local_path):
+    df = pd.read_csv(ndf_local_path, sep=";", parse_dates=["DATE"])
     return df
 
-def load_presentwx_data():
-    df = pd.read_csv("kcic_presentwx.csv", sep=";", parse_dates=["DATE"])
+def load_presentwx_data(pwx_local_path):
+    df = pd.read_csv(pwx_local_path, sep=";", parse_dates=["DATE"])
     return df
 
 def get_all_forecast(station_name, df):
@@ -132,11 +73,11 @@ def create_popup(row, df_fct):
 
 
 st.header("KCIC Weather Forecast Dashboard")
-df = merge_ndf_kecamatan()
-df_pwx = merge_presentwx()
 
-# df_pwx = load_presentwx_data()
-# df = load_forecast_data()
+pwx_local_path, ndf_local_path = download_data()
+df_pwx = load_presentwx_data(pwx_local_path)
+df_ndf = load_forecast_data(ndf_local_path)
+
 gdf_track = gpd.read_file('src/kcic.geojson')
 df_sta = pd.read_csv("src/track_ndf.csv")
 
@@ -167,19 +108,20 @@ if station_name:
     station_name = station_name.split('\n')
     station_name = f"{station_name[0]}"
     st.write(f"## Detailed Forecast for {station_name}")
-    df_all_fct = get_all_forecast(station_name, df)
+    df_all_fct = get_all_forecast(station_name, df_ndf)
     df_pwx = df_pwx.loc[df_pwx["NAME"] == station_name]
-    df_all_fct = pd.concat([df_pwx, df_all_fct])
-    df_all_fct = df_all_fct.sort_values(by=['DATE'])
-    df_all_fct['DATE'] = df_all_fct["DATE"] + timedelta(hours=7)
-    df_grp = df_all_fct.groupby(by=df_all_fct["DATE"].dt.date)
+    df_combine = pd.concat([df_pwx, df_all_fct])
+    df_combine = df_combine.sort_values(by=['DATE'])
+    df_combine['DATE'] = df_combine["DATE"] + timedelta(hours=7)
+    df_combine.drop_duplicates(subset=['DATE'], inplace=True, keep='last')
+    df_grp = df_combine.groupby(by=df_combine["DATE"].dt.date)
     tanggal_list = list(df_grp.groups.keys())
     tanggal_list_string = [x.strftime("%d %b | ") for x in tanggal_list]
     tabs = st.tabs(tanggal_list_string)
 
     for tab, tanggal in zip(tabs, tanggal_list):
         df_tgl = df_grp.get_group(tanggal)
-        cols = tab.columns(df_tgl.shape[0])
+        cols = tab.columns(8)#df_tgl.shape[0])
         for col, (index, data) in zip(cols, df_tgl.iterrows()):
             markdown_content = f"""
                         <strong>{(data["DATE"]).strftime("%H:%M WIB")}</strong><br>
