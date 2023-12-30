4fba090c1abd7f1779991cd1d27afdc41376dd21
Rifani <achmad.rifani.93@gmail.com>
2023-12-30 14:32:42 +0700

    Update app, adding features to integrate nowcast and add warnings based on nowcast.

--- kcic_app.py
+++ kcic_app.py
@@ -128,13 +128,13 @@ if station_name:
     pwx_time = df_pwx["DATE"].iloc[0] +timedelta(hours=7)
     df_stp = df_stp.loc[df_stp["NAME"] == station_name]
     df_stp['DATE'] = df_stp["DATE"] + timedelta(hours=7)
-    warning_text = make_warning(df_stp.loc[df_stp["DATE"] > datetime.now()])
+    warning_text = make_warning(df_stp)
     df_now = df_stp.resample('30T', on='DATE').agg({'NAME':keep_first,
                                                     'LON':keep_first,
                                                     'LAT':keep_first,
                                                     'WEATHER' : 'max'})
     df_now.reset_index(col_level="DATE", inplace=True)
-    df_now = df_now.loc[(df_now["DATE"] > datetime.now()) & (df_now["DATE"] < fct_time)]
+    df_now = df_now.loc[(df_now["DATE"] >= datetime.now()) & (df_now["DATE"] < fct_time)]
     df_pwx = df_pwx[['DATE','NAME', 'LON', 'LAT', 'WEATHER']]
     df_now = pd.concat([df_pwx, df_now], ignore_index=True)
     df_now.drop_duplicates(subset=['DATE'], inplace=True, keep='last')
